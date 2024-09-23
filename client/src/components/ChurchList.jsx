28c9723826bdd26cff7044210684ce8550586da5
Brennon Allen <brennon.t.allen@gmail.com>
2024-09-23 00:34:46 -0500

    init

--- client/src/components/ChurchList.jsx
+++ client/src/components/ChurchList.jsx
@@ -4,11 +4,11 @@ import { useState, useEffect } from 'react'
 export default function ChurchList() { 
     const [churchList, setChurchList] = useState([])
 
-    useEffect(() => {
-        axios.get('/')
-            .then(data => console.log(data))
-            .catch(error => console.log(error))
-    }, [])
+    // useEffect(() => {
+    //     axios.get('/')
+    //         .then(data => console.log(data))
+    //         .catch(error => console.log(error))
+    // }, [])
 
 
 
@@ -16,9 +16,9 @@ export default function ChurchList() {
         <div className="church--list">
             <h1>Church List</h1>
             <p>Awaiting coding on backend to make the form work, and also show what's already in the database.</p>
-            {churchList.map((item, index) => {
+            {/* {churchList.map((item, index) => {
                 return <h3 className="church--name" key={index}>{item.name}</h3>
-            })}
+            })} */}
         </div>
     )
 }
